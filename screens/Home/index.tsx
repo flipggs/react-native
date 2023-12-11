04d4380240978352d93b971e772a3e4e3029cc31
Felipe Siqueira <flipggs@gmail.com>
2023-12-11 20:42:52 -0300

    feat: included alert on video ended

--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,5 +1,10 @@
-import {View, ActivityIndicator, useWindowDimensions} from 'react-native';
-import YoutubeIFrame from 'react-native-youtube-iframe';
+import {
+  View,
+  ActivityIndicator,
+  useWindowDimensions,
+  Alert,
+} from 'react-native';
+import YoutubeIFrame, {PLAYER_STATES} from 'react-native-youtube-iframe';
 
 import {styles, VIDEO_HEIGHT, SCREEN_SPACE} from './styles';
 import {useState} from 'react';
@@ -9,6 +14,12 @@ export function Home() {
   const {width} = useWindowDimensions();
   const videoWidth = width - SCREEN_SPACE * 2;
 
+  const onChangeState = (state: string) => {
+    if (state === PLAYER_STATES.ENDED) {
+      Alert.alert('E ai curtiu?', 'Valeu');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.player}>
@@ -17,6 +28,7 @@ export function Home() {
           height={videoReady ? VIDEO_HEIGHT : 0}
           width={videoWidth}
           onReady={() => setVieoReady(true)}
+          onChangeState={onChangeState}
         />
         {!videoReady && <ActivityIndicator color="red" />}
       </View>
